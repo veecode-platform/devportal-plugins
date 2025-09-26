b7ff1a04e0189b4d87531db11280e3d34e66ba45
Andre Fernandes <andre@vertigo.com.br>
2025-09-26 07:02:43 -0300

    removed makeStyles from VeeCodeHomePage

--- workspace/homepage/plugins/veecode-homepage/src/components/VeeCodeHomePage.tsx
+++ workspace/homepage/plugins/veecode-homepage/src/components/VeeCodeHomePage.tsx
@@ -1,21 +1,22 @@
-import { makeStyles } from '@mui/styles';
+// import { makeStyles } from '@mui/styles';
 import { HeaderComponent } from './headerComponent/HeaderComponent';
 import { HomeGreeting } from './homeGretting/HomeGretting';
 import { HomeContent } from './homeContent/HomeContent';
 import { Page } from '@backstage/core-components';
 import Box from '@mui/material/Box';
 
-const useVeeCodeHomePageStyles = makeStyles(theme => ({
-  pageRoot: {
-    backgroundColor: theme.palette.background.default,
-  },
-}));
+// const useVeeCodeHomePageStyles = makeStyles(theme => ({
+//   pageRoot: {
+//     backgroundColor: theme.palette.background.default,
+//   },
+// }));
 
 export const VeeCodeHomePage = () => {
-  const { pageRoot } = useVeeCodeHomePageStyles();
+  // const { pageRoot } = useVeeCodeHomePageStyles();
   return (
-    <Page themeId="home" className={pageRoot}>
+    <Page themeId="home">
       <Box sx={{ 
+        backgroundColor: 'background.default',
         display: 'flex', 
         flexDirection: 'column', 
         width: '100%', 
