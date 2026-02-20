eaaa164564a46e3037a1cbf8f1a536e384820d67
Andre Fernandes <andre@vertigo.com.br>
2026-02-20 09:29:43 -0300

    add sample APIs

--- workspace/kong-tools/scripts/start-kong.sh
+++ workspace/kong-tools/scripts/start-kong.sh
@@ -7,6 +7,9 @@ else
   echo "VKDR infrastructure is READY, skipping \`vkdr infra up\`"
 fi
 vkdr kong install --default-ic -t 3.9.1 -m standard --label "vee.codes/cluster=local"
+echo "Installing sample API..."
+kubectl apply -f "$(dirname "$0")/../examples/k8s/"
 echo "Kong Admin API: http://manager.localhost:8000"
 echo "Kong Manager:   http://manager.localhost:8000/manager"
 echo "Kong Proxy:     http://localhost:8000"
+echo "Sample API:     http://localhost:8000/cep/24348000/json"
